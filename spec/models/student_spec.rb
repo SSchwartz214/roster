0b682bf98f497be3910574c4674cbab591638a58
Seth Schwartz <sschwartz214@gmail.com>
2018-07-10 14:42:57 -0600

    Adds show action and spec

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -4,19 +4,4 @@ describe Student, type: :model do
   describe "validations" do
     it {should validate_presence_of(:name)}
   end
-  describe 'user sees all students' do
-    describe 'they visit /students' do
-      it 'displays all students' do
-        student_1 = Student.create(name: "Seth", age: 30)
-        student_2 = Student.create(name: "Angela", age: 21)
-
-        visit '/students'
-
-        expect(page).to have_content(student_1.name)
-        expect(page).to have_content(student_1.age)
-        expect(page).to have_content(student_2.name)
-        expect(page).to have_content(student_2.age)
-      end
-    end
-  end
 end
