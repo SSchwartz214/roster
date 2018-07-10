ab957daee0ce827f2af5176275f7d222a722f8f7
Seth Schwartz <sschwartz214@gmail.com>
2018-07-10 15:18:39 -0600

    removes age attribute

--- spec/features/user_sees_a_student_spec.rb
+++ spec/features/user_sees_a_student_spec.rb
@@ -3,12 +3,11 @@ require 'rails_helper'
 describe 'user sees a student' do
   describe 'they visit /student' do
     it 'displays a student' do
-      student_1 = Student.create(name: "Seth", age: 30)
+      student_1 = Student.create(name: "Seth")
 
       visit student_path(student_1)
 
       expect(page).to have_content("Name: #{student_1.name}")
-      expect(page).to have_content("Age: #{student_1.age}")
     end
   end
 end
--- spec/features/user_sees_all_students_spec.rb
+++ spec/features/user_sees_all_students_spec.rb
@@ -3,15 +3,13 @@ require 'rails_helper'
 describe 'user sees all students' do
   describe 'they visit /students' do
     it 'displays all students' do
-      student_1 = Student.create(name: "Seth", age: 30)
-      student_2 = Student.create(name: "Angela", age: 21)
+      student_1 = Student.create(name: "Seth")
+      student_2 = Student.create(name: "Angela")
 
       visit students_path
 
       expect(page).to have_content("Name: #{student_1.name}")
-      expect(page).to have_content("Age: #{student_1.age}")
       expect(page).to have_content("Name: #{student_2.name}")
-      expect(page).to have_content("Age: #{student_2.age}")
     end
   end
 end
